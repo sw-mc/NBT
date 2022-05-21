a064a467dbd5b57abed7c0d16b5fc251d54c146b
Armen Deroian <cosmic5173@gmail.com>
2022-05-20 23:40:01 -0400

    Serialization

---
+++ TreeRoot.cs
@@ -0,0 +1,22 @@
+﻿using SkyWing.NBT.Tag;
+
+namespace DefaultNamespace; 
+
+public class TreeRoot {
+
+	public Tag Root { get; }
+	public string Name { get; }
+	
+	public TreeRoot(Tag root, string name) {
+		Root = root;
+		Name = name;
+	}
+
+	public bool Equals(TreeRoot that) {
+		return Name.Equals(that.Name) && Root.Equals(that.Root);
+	}
+
+	public override string ToString() {
+		return "ROOT {\n" + (Name != "" ? "\"" + Name + "\" => " : "") + Root._ToString(1) + "\n}";
+	}
+}
---
+++ serializer/NbtStream.cs
@@ -0,0 +1,55 @@
+﻿namespace SkyWing.NBT.Serializer;
+
+public interface NbtStreamReader {
+	
+	public byte ReadByte();
+
+	public sbyte ReadSignedByte();
+	
+	public ushort ReadShort();
+	
+	public short ReadSignedShort();
+	
+	public int ReadInt();
+	
+	public long ReadLong();
+	
+	public float ReadFloat();
+	
+	public double ReadDouble();
+	
+	public string ReadString();
+	
+	public byte[] ReadByteArray();
+	
+	public int[] ReadIntArray();
+}
+
+public interface NbtStreamWriter {
+	
+	public void WriteByte(byte value);
+
+	public void WriteByte(sbyte value);
+	
+	public void WriteShort(short value);
+	
+	public void WriteShort(ushort value);
+
+	public void WriteInt(int value);
+	
+	public void WriteInt(uint value);
+	
+	public void WriteLong(long value);
+	
+	public void WriteLong(ulong value);
+	
+	public void WriteFloat(float value);
+	
+	public void WriteDouble(double value);
+	
+	public void WriteString(string value);
+	
+	public void WriteByteArray(byte[] value);
+	
+	public void WriteIntArray(int[] value);
+}
