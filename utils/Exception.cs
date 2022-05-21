e26ee3111e94f3d23400c984da4cc6f997b2438c
Armen Deroian <cosmic5173@gmail.com>
2022-05-20 23:39:46 -0400

    Exceptions

---
+++ utils/Exception.cs
@@ -0,0 +1,29 @@
+﻿namespace SkyWing.NBT.Utils; 
+
+public class NbtException : Exception
+{
+	public NbtException(string message) : base(message)
+	{
+	}
+}
+
+public class NbtDataException : NbtException
+{
+	public NbtDataException(string message) : base(message)
+	{
+	}
+}
+
+public class UnexpectedTagTypeException : NbtDataException
+{
+	public UnexpectedTagTypeException(string message) : base(message)
+	{
+	}
+}
+
+public class NoSuchTagException : NbtDataException
+{
+	public NoSuchTagException(string message) : base(message)
+	{
+	}
+}
