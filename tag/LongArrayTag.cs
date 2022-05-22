f9ea7cffe883ce004625b4eb6c945ca4f9753f48
Armen Deroian <cosmic5173@gmail.com>
2022-05-21 23:21:14 -0400

    Long array tag

---
+++ tag/LongArrayTag.cs
@@ -0,0 +1,36 @@
+﻿using SkyWing.NBT.Serialization;
+
+namespace SkyWing.NBT.Tag;
+
+public class LongArrayTag : ImmutableTag {
+
+	private readonly long[] _value;
+
+	public LongArrayTag(long[] value) {
+		_value = value;
+	}
+
+	public override long[] GetValue() {
+		return _value;
+	}
+
+	public override byte GetTagType() {
+		return (byte) TagType.LongArray;
+	}
+
+	public override void Write(NbtStreamWriter writer) {
+		writer.WriteLongArray(_value);
+	}
+	
+	public static LongArrayTag Read(NbtStreamReader reader) {
+		return new LongArrayTag(reader.ReadLongArray());
+	}
+
+	public override string GetTypeName() {
+		return "LongArray";
+	}
+
+	public override string StringifyValue(int indentation) {
+		return "implode:" + string.Join('.', _value);
+	}
+}
